f397204dabba699f3b6f6a074a8426cf001a0ab1
Emily Tschida <thetschida@gmail.com>
2021-06-24 19:48:04 -0500

    SplashPage component. State unresolved.

--- client/App.js
+++ client/App.js
@@ -1,9 +1,23 @@
 /** @format */
-
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import package modules
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
+
+// import CSS
+import './App.css';
+
+// import components
+import SplashPage from './components/SplashPage';
+
+// react router
 const App = () => {
-  return <div>Hi there </div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={SplashPage}/>
+      </Switch>
+    </BrowserRouter>
+  )
 };
 
 export default App;
