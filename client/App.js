3efce1c25db2eafc639abe42215bbffb79be61ad
Wes <wesget182@gmail.com>
2021-06-26 10:25:14 -0700

    Fixed merge conflicts in App.js

--- client/App.js
+++ client/App.js
@@ -2,13 +2,7 @@
 // import package modules
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
-<<<<<<< HEAD
 
-=======
-import Navbar from './components/Navbar';
-import BurialPlan from './components/BurialPlan';
-import ServicePlan from './components/ServicePlan';
->>>>>>> 984ceb94b21e011ff33a4c8c03cb13798537d478
 // import CSS
 import './App.css';
 // import components
@@ -17,6 +11,7 @@ import SplashPage from './components/SplashPage';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import BurialPlan from './components/BurialPlan';
+import ServicePlan from './components/ServicePlan';
 // react router
 const App = () => {
   return (
@@ -25,12 +20,9 @@ const App = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={SplashPage} />
-<<<<<<< HEAD
           <Route exact path='/login' component={Login} />
           <Route exact path='/dashboard' component={Dashboard} />
           <Route exact path='/burial-plan' component={BurialPlan} />
-=======
->>>>>>> 984ceb94b21e011ff33a4c8c03cb13798537d478
         </Switch>
       </BrowserRouter>
     </div>
