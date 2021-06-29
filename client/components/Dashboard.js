ed59b17023614e668cde130952400fb89017a9ab
Kailee Pedersen <kaileepedersen@Kailees-Mac-mini.local>
2021-06-28 22:50:39 -0400

    preliminary work on conditionally rendering dashboard

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,11 +1,27 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import DashboardSquare from './DashboardSquare';
 import { Button } from '@material-ui/core';
 import '../css/Dashboard.css';
+import { userInfoState } from '../slices/userInfoSlice';
 
 const Dashboard = () => {
+  const state = useSelector(userInfoState);
+  const history = useHistory();
+
+  const [squareHidden, toggleSquareHidden] = useState({
+    burialPlan: false,
+    service: false,
+    futureChecklist: false
+  })
+  const userId = state.userInfo.userId;
+
+  //check database using userId to see if dashboard square is rendered or not
+  
+
   return (
     <div id='dashboard-container'>
       <h1>Your Journey</h1>
