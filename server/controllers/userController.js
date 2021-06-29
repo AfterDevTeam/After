37f0173320e0ec4d72702b0d561b27eb192cb508
Murphypie <Ochemypie@gmail.com>
2021-06-29 18:27:09 -0400

    bcrypt done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,9 @@
 /** @format */
 
 const db = require('../models/afterModels.js');
+const bcrypt = require('bcrypt')
+
+const SaltFactor = 5;
 
 const userController = {};
 
@@ -17,16 +20,18 @@ userController.getAllUsers = async (req, res, next) => {
 
 userController.verifyUser = async (req, res, next) => {
   try {
-    const userQuery = `SELECT * FROM userinfo WHERE email = '${req.body.email}'`;
-    const userValid = await db.query(userQuery);
+    const userQuery = `SELECT * FROM userinfo WHERE email = $1`;
+    const values = [req.body.email];
+    const userValid = await db.query(userQuery, values);
 
+    console.log(values, userValid.rows[0])
     if (userValid.rows.length === 0) {
       return next();
     } else {
       const { firstname, lastname, email, password, user_id } =
         userValid.rows[0];
 
-      if (password === req.body.password) {
+      if (bcrypt.compare(req.body.password, userValid.rows[0].password)) {
         res.locals.userInfo = {
           firstName: firstname,
           lastName: lastname,
@@ -45,10 +50,17 @@ userController.verifyUser = async (req, res, next) => {
 
 userController.createUser = async (req, res, next) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
-    const value = [firstName, lastName, email, password];
+    const { firstName, lastName, email} = req.body;
+    let {password} = req.body
     const queryText = `SELECT * FROM userinfo WHERE email = '${email}'`;
     const queryResult = await db.query(queryText);
+    
+    const salt = await bcrypt.genSalt(SaltFactor);
+    const hash = await bcrypt.hash(password, salt);
+    password = hash;
+    
+    const value = [firstName, lastName, email, password];
+
     if (queryResult.rowCount === 0) {
       const addText =
         'INSERT INTO userinfo (firstName, lastName, email, password) values($1,$2,$3,$4)';
