7528d8f45534298bc363fdc693ae812216060a76
Gordon Hui <randomizer@macbook-pro.lan>
2021-06-28 23:27:46 -0400

    complete update functionality for burial plan

--- server/routes/api.js
+++ server/routes/api.js
@@ -26,26 +26,44 @@ router.get('/future', afterController.getFuture, (req, res) => {
 
 //  post routes
 router.post('/plan', afterController.addPlan, (req, res) => {
-  res.status(200).send('');
+  res.status(200).send('Successfully added to plan');
 });
 router.post('/service', afterController.addService, (req, res) => {
-  res.status(200).send('');
+  res.status(200).send('Successfully added to service');
 });
 router.post('/future', afterController.addFuture, (req, res) => {
-  res.status(200).send('');
+  res.status(200).send('Successfully added to future');
 });
 
 // delete routes
+router.delete('/plan', afterController.deletePlan, (req, res) => {
+  res.status(200).send('Entry was deleted Successfully');
+});
+router.delete('/service', afterController.deleteService, (req, res) => {
+  res.status(200).send('Entry was deleted Successfully');
+});
+router.delete('/future', afterController.deleteFuture, (req, res) => {
+  res.status(200).send('Entry was deleted Successfully');
+});
 
 //  update routes
+router.put('/plan', afterController.updatePlan, (req, res) => {
+  res.status(200).send('Update to plan was successful');
+});
+// router.put('/service', afterController.updateService, (req, res) => {
+//   res.status(200).send('Update to service was successful');
+// });
+// router.put('/future', afterController.updateFuture, (req, res) => {
+//   res.status(200).send('Update to future was successful');
+// });
 
-router.post(
-  '/guestlist/:id',
-  multipleInsertController.populateGuestList,
-  (req, res) => {
-    res.status(200).send('Created Guest List');
-  }
-);
+// router.post(
+//   '/guestlist/:id',
+//   multipleInsertController.populateGuestList,
+//   (req, res) => {
+//     res.status(200).send('Created Guest List');
+//   }
+// );
 
 //consider moving these into routers/user.js
 // router.post('/register', afterController.registerUser, (req, res) =>{
